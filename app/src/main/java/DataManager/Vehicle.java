cc19a0ba8089ee966ec813f10aac83846642a9d6
CesarValdez2108 <85514996+CesarValdez2108@users.noreply.github.com>
2022-04-04 23:33:09 -0700

    Update Vehicle.java
    Added vehicle characteristics and set/get for each characteristic.

--- app/src/main/java/DataManager/Vehicle.java
+++ app/src/main/java/DataManager/Vehicle.java
@@ -1,20 +1,75 @@
 package DataManager;
+
 import DataManager.Pool.*;
 
-public class Vehicle extends Component{
-    //TODO: Add fields and get/set methods
-    
-    public Vehicle(Model model) throws ComponentNotBoundToPool{
-        if(model == null);
-        if(!ModelPool.get().componentIsRegisteredAtPool(model)){
-            throw new ComponentNotBoundToPool("The 'model' object passed to bindToModel does not exist inside ModelPool");
+public class Vehicle extends Component {
+    // TODO: Add fields and get/set methods
+    private String vehicleName = new String();
+    private Boolean quemacocos = false;
+    private Integer numPuertas = 0;
+    private Integer asientos = 0;
+    private Double kilometraje = 0.0;
+    private Double gasolina = 0.0;
+
+    public Vehicle(Model model) throws ComponentNotBoundToPool {
+        if (model == null)
+            ;
+        if (!ModelPool.get().componentIsRegisteredAtPool(model)) {
+            throw new ComponentNotBoundToPool(
+                    "The 'model' object passed to bindToModel does not exist inside ModelPool");
         }
         model.addChild(this);
     }
-    
-    public Model getModel(){
-        return (Model)getParent();
+
+    public Model getModel() {
+        return (Model) getParent();
     }
-    
-    
+
+    public String getVehicleName() {
+        return this.vehicleName;
+    }
+
+    public void setVehicleName(String name) {
+        this.vehicleName = name;
+    }
+
+    public Boolean getQuemacocos() {
+        return this.quemacocos;
+    }
+
+    public void setQuemacocos(Boolean cocos) {
+        this.quemacocos = cocos;
+    }
+
+    public Integer getNumPuertas() {
+        return numPuertas;
+    }
+
+    public void setNumPuertas(Integer puertas) {
+        this.numPuertas = puertas;
+    }
+
+    public Integer getAsientos() {
+        return asientos;
+    }
+
+    public void setAsientos(Integer asientos) {
+        this.asientos = asientos;
+    }
+
+    public Double getKilometraje() {
+        return kilometraje;
+    }
+
+    public void setKilometraje(Double kilometraje) {
+        this.kilometraje = kilometraje;
+    }
+
+    public Double getGasolina() {
+        return gasolina;
+    }
+
+    public void setGasolina(Double gasolina) {
+        this.gasolina = gasolina;
+    }
 }
